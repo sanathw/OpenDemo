46c221a73ddf6d2f3a48419410f3140aff18e1fb
sanathw <sanath.wanigaratne@gmail.com>
2026-05-14 13:40:31 +1000

    B
    B

---
+++ Abby/_apps.js
@@ -0,0 +1,5 @@
+var apps = 
+[
+{id:'AbbyBridgeScorer', name:'Abby's Bridge Scorer', path:'./AbbyBridgeScorer', alts:['one'] },
+{id:'PhyllotaxisFermatsSpiral', name:'Sunflower', path:'./PhyllotaxisFermatsSpiral', alts:['one'] }
+];
